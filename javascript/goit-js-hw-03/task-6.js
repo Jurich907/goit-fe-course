89043fd97827b01ee13e05478682181384c18a49
Jurich907 <jurich907@gmail.com>
2019-10-07 14:19:18 +0300

    added

---
+++ javascript/goit-js-hw-03/task-6.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 }
+];
+
+const calculateTotalPrice = function(arr, productName) {
+  const values = Object.values(arr);
+  let total = 0;
+
+  for (const value of values) {
+    if (productName === value.name) {
+      return (total = value.price * value.quantity);
+    }
+  }
+};
+
+console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+console.log(calculateTotalPrice(products, "Дроид")); // 2800
